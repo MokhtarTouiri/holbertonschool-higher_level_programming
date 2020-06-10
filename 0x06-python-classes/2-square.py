5cc4907edda25a7529c5cb1bf80b5b82c1468e73
MokhtarTouiri <design.solutionts.tn@gmail.com>
2020-06-10 14:04:28 +0000

    fix

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module """
+
+
+class Square:
+	""" Empty Class """
+	def __init__(self, size=0):
+		""" Mokhtar """
+		if not type(size) is int:
+			raise TypeError('size must be an integer')
+		if size < 0:
+			raise ValueError('size must be >= 0')
+		self.__size = size
