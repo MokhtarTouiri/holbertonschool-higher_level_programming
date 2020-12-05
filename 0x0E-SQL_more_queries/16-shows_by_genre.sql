c58c4463b320f96d8809445ec87067072c2f8075
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-05 20:26:48 +0000

    fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,8 @@
 -- This Code Created By Mokhtar Tiouiri
 -- More SQL!
 -- More SQL!
-SELECT tv_shows.title 
-FROM tv_show_genres 
-JOIN tv_genres 
-ON tv_genres.name = 'Comedy' AND tv_show_genres.genre_id = tv_genres.id 
-INNER JOIN tv_shows 
-ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title;
+SELECT tv_shows.title AS title, tv_genres.name AS name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_genres.name;
