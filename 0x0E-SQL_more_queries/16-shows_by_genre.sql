03c8b47408ac5bb14e4057e1886138d3a37b83bb
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-05 20:25:30 +0000

    fix

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- This Code Created By Mokhtar Tiouiri
+-- More SQL!
+-- More SQL!
+SELECT tv_shows.title 
+FROM tv_show_genres 
+JOIN tv_genres 
+ON tv_genres.name = 'Comedy' AND tv_show_genres.genre_id = tv_genres.id 
+INNER JOIN tv_shows 
+ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title;
