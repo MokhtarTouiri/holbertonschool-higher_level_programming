9d02486a4df7c592557da0148889a77ec476de98
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-05 20:13:22 +0000

    fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,6 @@
 -- More SQL!
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id 
 FROM tv_shows 
-JOIN tv_show_genres WHERE tv_shows.id = tv_show_genres.show_id 
+JOIN tv_show_genres 
+WHERE tv_shows.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
