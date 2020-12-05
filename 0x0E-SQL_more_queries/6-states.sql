4a6a77362c4a5f3989d0eaff95f35bedae46239b
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-05 20:00:56 +0000

    fix

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- This Code Created By Mokhtar Tiouiri
+-- More SQL!
+-- More SQL!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+id INT 
+NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
