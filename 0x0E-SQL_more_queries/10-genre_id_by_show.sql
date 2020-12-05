4e4dceef7c4d28db0e160e23119fde90c2003c8e
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-05 20:16:48 +0000

    fix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
 -- This Code Created By Mokhtar Tiouiri
 -- More SQL!
 -- More SQL!
-SELECT cities.id AS id, cities.name AS name, states.name AS name FROM cities JOIN states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
-
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id 
+FROM tv_shows 
+JOIN tv_show_genres WHERE tv_shows.id = tv_show_genres.show_id 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,3 +5,4 @@ SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows 
 JOIN tv_show_genres 
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
+
