b804a5864477ec806d8e578d106e19b4eace4491
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-05 19:48:01 +0000

    fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- This Code Created By Mokhtar Tiouiri
 -- More SQL!
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2; 
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
+-- More SQL!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2; GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
 IDENTIFIED BY 'user_0d_2pwd';
