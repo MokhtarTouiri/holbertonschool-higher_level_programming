140a7d37dd0bb569e1dd2b728cbb68f059740457
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-05 20:09:13 +0000

    fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,4 @@
 -- This Code Created By Mokhtar Tiouiri
 -- More SQL!
 -- More SQL!
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
