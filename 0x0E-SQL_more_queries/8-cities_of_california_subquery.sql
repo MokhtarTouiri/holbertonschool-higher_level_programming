8adf98279b88e0000a438065ace75ffc522d2fc7
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-05 20:06:47 +0000

    fix

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- This Code Created By Mokhtar Tiouiri
+-- More SQL!
+-- More SQL!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+id INT NOT NULL 
+PRIMARY KEY AUTO_INCREMENT, 
+state_id INT NOT NULL, 
+FOREIGN KEY (state_id) REFERENCES states(id), 
+name VARCHAR(256) NOT NULL);
