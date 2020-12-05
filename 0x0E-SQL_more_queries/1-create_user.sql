ab964a6d001262838479416781a363294564a3a9
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-05 19:46:26 +0000

    fix

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- This Code Created By Mokhtar Tiouiri
+-- More SQL!
+GRANT ALL ON * . * TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- This Code Created By Mokhtar Tiouiri
+-- More SQL!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2; GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
+IDENTIFIED BY 'user_0d_2pwd';
