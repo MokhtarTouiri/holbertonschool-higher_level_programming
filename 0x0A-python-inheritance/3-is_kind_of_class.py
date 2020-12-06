d024d8a7b46ec6e39b304102ce3ad184682630ea
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-06 13:34:27 +0000

    fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def is_kind_of_class(obj, a_class):
-    if issubclass(type(obj), a_class):
-        return True
-    return False
+""" Module """
 
+
+def is_kind_of_class(obj, a_class):
+    """ models """
+    return isinstance(obj, a_class)
