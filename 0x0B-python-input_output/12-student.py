bff8e7d43751ac85d815b3568f7d42ae87f2a304
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-06 18:18:22 +0000

    fix

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module """
+
+
+class Student:
+    """ DEF INIT """
+    def __init__(self, first_name, last_name, age):
+        """ SELF """
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+    """ DEF JSON """
+    def to_json(self, attrs=None):
+        if attrs is None:
+            """ RETURN SELF """
+            return self.__dict__
+        d = {}
+        for x in attrs:
+            """ IF """
+            if x in self.__dict__:
+                d[x] = self.__dict__[x]
+        """ Return d """
+        return d
