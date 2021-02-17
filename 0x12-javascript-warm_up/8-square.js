0a87d41de82e17b230a24f95ce6336e067e7480c
mokhtartouiri <design.solutions.tn@gmail.com>
2021-02-17 13:29:04 +0000

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
-for (let y = 0; y < size; y++) {
-for (let z = 0; z < size; z++) {
-    let i = '';
+let i = '';
+const s = parseInt(process.argv[2]);
+for (let a = 0; a < s; a++) {
+  for (let b = 0; b < s; b++) {
     i += 'X';
   }
   console.log(i);
-    i = '';
+  i = '';
 }
 if (process.argv[2] === 0 || isNaN(process.argv[2])) {
   console.log('Missing size');
