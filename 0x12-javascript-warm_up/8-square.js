c1f2db400afe8096a5114ea46440c01a1c8e0a70
mokhtartouiri <design.solutions.tn@gmail.com>
2021-02-17 13:24:17 +0000

    fix

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+for (let y = 0; y < size; y++) {
+  for (let z = 0; z < size; z++) {
+    let i = 0;
+    i += 'X';
+  }
+  console.log(i);
+    i = '';
+}
+if (process.argv[2] === 0 || isNaN(process.argv[2])) {
+  console.log('Missing size');
+}
