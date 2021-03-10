e7fa6116b818473664a50244389dd6dc48fa1efd
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-10 16:47:39 +0000

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const process = require('process');
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
-  if (err) {
-    console.log(err);
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, res, body) {
+  if (error) {
+    console.log(error);
   }
   const json = JSON.parse(body);
   console.log(json.title);
 });
-
