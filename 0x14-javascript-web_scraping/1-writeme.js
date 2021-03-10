025faf4cb16dac9234a739bcaff95cc4d6712d8b
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-10 15:46:45 +0000

    fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
+const argv = process.argv;
 fs.writeFile(argv[2], argv[3], 'utf-8', function (error) {
-  if (error) { console.log(error); }
+  if (error) { 
+    console.log(error); 
+  }
 });
