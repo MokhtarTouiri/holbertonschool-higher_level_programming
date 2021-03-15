b5362d0ebd65653c4d834a1452e4d3a1d1d6f502
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-15 13:49:01 +0000

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,13 @@
 
 const process = require('process');
 const request = require('request');
-request(process.argv[2], function (err, res, body) {
+request(process.argv[2], function (err, response, body) {
   if (error) { throw error; }
-  const jsn = JSON.parse(body);
+  const json = JSON.parse(body);
   let z = 0;
-  for (let i = 0; jsn.results[i]; i++) {
-    for (let y = 0; jsn.results[i].characters[y]; y++) {
-      if (jsn.results[i].characters[y].includes('/api/people/18/')) { z++; }
+  for (let i = 0; json.results[i]; i++) {
+    for (let y = 0; json.results[i].characters[y]; y++) {
+      if (json.results[i].characters[y].includes('/api/people/18/')) { z++; }
     }
   }
   console.log(z);
