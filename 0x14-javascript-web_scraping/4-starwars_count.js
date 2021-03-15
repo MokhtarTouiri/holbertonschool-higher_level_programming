0fdcf1aa19b9b4c74ad0148687706a4674854979
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-15 13:39:35 +0000

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+
 const process = require('process');
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
+request(process.argv[2], function (err, res, body) {
   if (error) { throw error; }
   const jsn = JSON.parse(body);
   let z = 0;
