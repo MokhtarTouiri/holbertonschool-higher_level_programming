77f63d45590cb99d80118eb776cc3f7dc2304ebb
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-16 23:50:27 +0000

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,3 +1,16 @@
 #!/usr/bin/node
 const process = require('process');
-
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+    let X = 0;
+    if (error) {
+      console.log(error);
+    }
+    const js = JSON.parse(body);
+    for (let i = 0; js.results[i] !== undefined; i++) {
+      if (js.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        x++;
+      }
+    }
+    console.log(x);
+  });
