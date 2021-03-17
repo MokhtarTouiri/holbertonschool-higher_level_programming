d2387d54ecc1ee971dcd6392a6fd29f29748b896
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-17 00:15:29 +0000

    fix

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+const request = require('request');
+
+request(process.argv[2], function (err, res, body) {
+  if (err) {
+    console.log(error);
+  } fs.writeFile(process.argv[3], body, 'utf8', (err) => { 
+    if (err) console.log(err); 
+});
+});
