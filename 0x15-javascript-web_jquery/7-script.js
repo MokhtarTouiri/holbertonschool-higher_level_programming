537f07d27c926768d803e3a0641569211dfa6774
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-17 01:31:32 +0000

    fix

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+const $ = window.$;
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (data, textStatus) => {
+    if (textStatus === 'success') {
+      $('DIV#character').text(data.name);
+
+    }
+  });
+});
+
