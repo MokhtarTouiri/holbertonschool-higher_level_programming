dfa1fe5f49dc0e3d2ee88869e90bdfa777640e10
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-17 00:40:05 +0000

    fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function(error, res, body) {
+request(url, function (error, response, body) {
   const Any = {};
   const json = JSON.parse(body);
   for (const ex of json) {
