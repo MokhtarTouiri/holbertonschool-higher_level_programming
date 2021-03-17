c6893f69a00e0007aab0b0d89ae0dd04297a20a6
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-17 00:32:01 +0000

    fix

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, res, body) {
+  const Any = {};
+  const json = JSON.parse(body);
+  for (const ex of json) {
+    if (ex.completed === true) {
+      if (Any[ex.userId]) {
+    Any[ex.userId] += 1;
+    } else {
+    Any[ex.userId] = 1;
+      }
+    }
+  }
+  console.log(Any);
+});
