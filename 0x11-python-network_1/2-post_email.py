0084310da9b0510ec567070b06898e3f66985599
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-20 20:32:47 +0000

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,6 +7,6 @@ from sys import argv
 from urllib import parse
 from urllib import request
 if __name__ == "__main__":
-    value = parse.urlencode({"email": sys.argv[2]}).encode()
-    with request.urlopen(sys.argv[1], value) as response:
+    values = parse.urlencode({"email": argv[2]}).encode()
+    with request.urlopen(argv[1], values) as response:
         print(response.read().decode('utf-8'))
