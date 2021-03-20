e7ee7b6a2f83ce62f05016455fb22d6f1b1c6dfc
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-20 19:59:02 +0000

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" Python script that fetches """
+"""
+Python script that fetches
+"""
+from urllib import request
 
-
-if __name__ == "__main__":
-    import request.urllib
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+with request.urlopen('https://intranet.hbtn.io/status') as response:
         read = response.read()
         print('Body response:')
-        print('\t- content: {}'.format(read))
         print('\t- type: {}'.format(type(read)))
+        print('\t- content: {}'.format(read))
         print('\t- utf8 content: {}'.format(read.decode("utf-8", "replace")))
