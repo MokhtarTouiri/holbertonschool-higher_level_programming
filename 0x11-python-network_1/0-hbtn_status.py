810bff948893cf1ada6f089ec7252b51e7dcdd08
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-20 20:13:10 +0000

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Python script that fetches
+0. What's my status? #0
 """
 from urllib import request
-
-with request.urlopen('https://intranet.hbtn.io/status') as response:
+if __name__ == '__main__':
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(read.decode("utf-8")))
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('utf-8'))
