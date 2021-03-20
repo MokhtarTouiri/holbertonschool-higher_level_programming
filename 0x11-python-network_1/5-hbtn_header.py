4225386a6700a1ba508e06663ff933bc8944b4f6
mokhtartouiri <design.solutions.tn@gmail.com>
2021-03-20 20:56:40 +0000

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,7 +2,7 @@
 """5. Response header value #1
 """
 import requests
-import sys.argv
+import sys
 if __name__ == '__main__':
-    response = requests.get(argv[1])
+    response = requests.get(sys.argv[1])
     print(response.headers.get('X-Request-Id'))
