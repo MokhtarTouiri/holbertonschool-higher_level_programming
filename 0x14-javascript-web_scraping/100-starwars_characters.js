7a5a82e15d2c30d642db69cb9724a9762b664a6d
MokhtarTouiri <59121449+MokhtarTouiri@users.noreply.github.com>
2021-06-09 01:37:12 +0100

    fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,20 @@
- i
+#!/usr/bin/node
+""" Who was playing in this movie """
+
+const request = require('request');
+const movie = process.argv[2];
+const url = 'https://swapi.co/api/films/' + movie;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const dict = JSON.parse(body);
+  const characters = dict.characters;
+  characters.forEach(character => {
+    request(character, (error, response, data) => {
+      if (error) throw error;
+      console.log(JSON.parse(data).name);
+    });
+  });
+});
